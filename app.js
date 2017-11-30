55fb9b188fea70e874d1576d4f151affabddac32
Muganwa <stevenmuganwa@live.com>
2017-11-30 12:57:00 +0300

    json and global variables

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+/*
+*declaring a global variable
+*app.locals.videodata = require('./videodata.json');*/
 
 app.set('view engine', 'ejs');
 //app.use(express.static('routes'))/**using static routing for a whole folder */
 app.get('/', routes.home);
 app.get('/about', routes.about);
 
-app.listen(3000, () => console.log('app listening on port 3000!'))
+app.listen(3000, () => console.log('app listening on port 3000!'));
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const home_title = "Muganwas Express";
 const about_title = "About Muganwas";
+const vd = require('../videodata.json');
 
 module.exports = {
-    home: (req, res)=>{res.render('index', {title: home_title})},
+    home: (req, res)=>{res.render('index', {title: home_title,videodata:vd})},
     about: (req, res)=>{res.render('about', {title: about_title})},
 }
